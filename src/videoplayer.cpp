288593c5732b6ea42f2484bbf5cc178a881e498d
Ahmed Nadeem <ahmn21@hotmail.com>
2021-07-01 14:54:48 +0500

    Stop video

--- src/videoplayer.cpp
+++ src/videoplayer.cpp
@@ -1,86 +1,204 @@
 #include "videoplayer.h"
 
 #include <iostream>
+#include <algorithm>
 
-void VideoPlayer::numberOfVideos() {
+void VideoPlayer::numberOfVideos()
+{
   std::cout << mVideoLibrary.getVideos().size() << " videos in the library"
             << std::endl;
 }
 
-void VideoPlayer::showAllVideos() {
-  std::cout << "showAllVideos needs implementation" << std::endl;
+void VideoPlayer::showAllVideos()
+{
+  std::vector<std::string> videosStringVector;
+  std::string videoIdNameTagString;
+  std::cout << "Here's a list of all available videos:" << std::endl;
+
+  for (Video itr : mVideoLibrary.getVideos())
+  {
+    std::vector<std::string> temp = itr.getTags();
+
+    videoIdNameTagString = itr.getTitle() + " (" + itr.getVideoId() + ") [";
+
+    for (std::string stItr : temp)
+    {
+      videoIdNameTagString.append(stItr + " ");
+    }
+
+    if (videoIdNameTagString.back() == ' ')
+      videoIdNameTagString.pop_back();
+
+    videoIdNameTagString += "]";
+    videosStringVector.push_back(videoIdNameTagString);
+    videoIdNameTagString.clear();
+  }
+
+  std::sort(videosStringVector.begin(), videosStringVector.end());
+
+  for (std::string stItr : videosStringVector)
+  {
+    std::cout << stItr << std::endl;
+  }
+  videosStringVector.clear();
 }
 
-void VideoPlayer::playVideo(const std::string& videoId) {
-  std::cout << "playVideo needs implementation" << std::endl;
+void VideoPlayer::playVideo(const std::string &videoId)
+{
+  bool videoFound = false;
+  for (Video itr : mVideoLibrary.getVideos())
+  {
+    if (itr.getVideoId() == videoId)
+    {
+      videoFound = true;
+      break;
+    }
+  }
+  if (!videoFound)
+  {
+    std::cout << "Cannot play video: Video does not exist" << std::endl;
+    return;
+  }
+  else
+  {
+    if (!videoPlaying)
+    {
+      videoPlaying = true;
+      playingVideoName = videoId;
+      for (Video itr : mVideoLibrary.getVideos())
+      {
+        if (itr.getVideoId() == videoId)
+        {
+          std::cout << "Playing video: " << itr.getTitle() << std::endl;
+          return;
+        }
+      }
+    }
+    else
+    {
+      for (Video itr : mVideoLibrary.getVideos())
+      {
+        if (itr.getVideoId() == playingVideoName)
+        {
+          std::cout << "Stopping video: " << itr.getTitle() << std::endl;
+          break;
+        }
+      }
+      for (Video itr : mVideoLibrary.getVideos())
+      {
+        if (itr.getVideoId() == videoId)
+        {
+          std::cout << "Playing video: " << itr.getTitle() << std::endl;
+          return;
+        }
+      }
+    }
+  }
 }
 
-void VideoPlayer::stopVideo() {
-  std::cout << "stopVideo needs implementation" << std::endl;
+void VideoPlayer::stopVideo()
+{
+  if (!videoPlaying)
+  {
+    std::cout << "Cannot stop video: No video is currently playing" << std::endl;
+  }
+  else
+  {
+    for (Video itr : mVideoLibrary.getVideos())
+    {
+      if (itr.getVideoId() == playingVideoName)
+      {
+        std::cout << "Stopping video: " << itr.getTitle() << std::endl;
+        videoPlaying = false;
+        return;
+      }
+    }
+  }
 }
 
-void VideoPlayer::playRandomVideo() {
-  std::cout << "playRandomVideo needs implementation" << std::endl;
+void VideoPlayer::playRandomVideo()
+{
+  if(mVideoLibrary.getVideos().size() == 0)
+  {
+    std::cout << "No videos available" << std::endl;
+    return;
+  }
+  
 }
 
-void VideoPlayer::pauseVideo() {
+void VideoPlayer::pauseVideo()
+{
   std::cout << "pauseVideo needs implementation" << std::endl;
 }
 
-void VideoPlayer::continueVideo() {
+void VideoPlayer::continueVideo()
+{
   std::cout << "continueVideo needs implementation" << std::endl;
 }
 
-void VideoPlayer::showPlaying() {
+void VideoPlayer::showPlaying()
+{
   std::cout << "showPlaying needs implementation" << std::endl;
 }
 
-void VideoPlayer::createPlaylist(const std::string& playlistName) {
+void VideoPlayer::createPlaylist(const std::string &playlistName)
+{
   std::cout << "createPlaylist needs implementation" << std::endl;
 }
 
-void VideoPlayer::addVideoToPlaylist(const std::string& playlistName,
-                                     const std::string& videoId) {
+void VideoPlayer::addVideoToPlaylist(const std::string &playlistName,
+                                     const std::string &videoId)
+{
   std::cout << "addVideoToPlaylist needs implementation" << std::endl;
 }
 
-void VideoPlayer::showAllPlaylists() {
+void VideoPlayer::showAllPlaylists()
+{
   std::cout << "showAllPlaylists needs implementation" << std::endl;
 }
 
-void VideoPlayer::showPlaylist(const std::string& playlistName) {
+void VideoPlayer::showPlaylist(const std::string &playlistName)
+{
   std::cout << "showPlaylist needs implementation" << std::endl;
 }
 
-void VideoPlayer::removeFromPlaylist(const std::string& playlistName,
-                                     const std::string& videoId) {
+void VideoPlayer::removeFromPlaylist(const std::string &playlistName,
+                                     const std::string &videoId)
+{
   std::cout << "removeFromPlaylist needs implementation" << std::endl;
 }
 
-void VideoPlayer::clearPlaylist(const std::string& playlistName) {
+void VideoPlayer::clearPlaylist(const std::string &playlistName)
+{
   std::cout << "clearPlaylist needs implementation" << std::endl;
 }
 
-void VideoPlayer::deletePlaylist(const std::string& playlistName) {
+void VideoPlayer::deletePlaylist(const std::string &playlistName)
+{
   std::cout << "deletePlaylist needs implementation" << std::endl;
 }
 
-void VideoPlayer::searchVideos(const std::string& searchTerm) {
+void VideoPlayer::searchVideos(const std::string &searchTerm)
+{
   std::cout << "searchVideos needs implementation" << std::endl;
 }
 
-void VideoPlayer::searchVideosWithTag(const std::string& videoTag) {
+void VideoPlayer::searchVideosWithTag(const std::string &videoTag)
+{
   std::cout << "searchVideosWithTag needs implementation" << std::endl;
 }
 
-void VideoPlayer::flagVideo(const std::string& videoId) {
+void VideoPlayer::flagVideo(const std::string &videoId)
+{
   std::cout << "flagVideo needs implementation" << std::endl;
 }
 
-void VideoPlayer::flagVideo(const std::string& videoId, const std::string& reason) {
+void VideoPlayer::flagVideo(const std::string &videoId, const std::string &reason)
+{
   std::cout << "flagVideo needs implementation" << std::endl;
 }
 
-void VideoPlayer::allowVideo(const std::string& videoId) {
+void VideoPlayer::allowVideo(const std::string &videoId)
+{
   std::cout << "allowVideo needs implementation" << std::endl;
 }
--- src/videoplayer.h
+++ src/videoplayer.h
@@ -7,39 +7,42 @@
 /**
  * A class used to represent a Video Player.
  */
-class VideoPlayer {
- private:
+class VideoPlayer
+{
+private:
   VideoLibrary mVideoLibrary;
+  bool videoPlaying;
+  std::string playingVideoName;
 
- public:
+public:
   VideoPlayer() = default;
 
   // This class is not copyable to avoid expensive copies.
-  VideoPlayer(const VideoPlayer&) = delete;
-  VideoPlayer& operator=(const VideoPlayer&) = delete;
+  VideoPlayer(const VideoPlayer &) = delete;
+  VideoPlayer &operator=(const VideoPlayer &) = delete;
 
   // This class is movable.
-  VideoPlayer(VideoPlayer&&) = default;
-  VideoPlayer& operator=(VideoPlayer&&) = default;
+  VideoPlayer(VideoPlayer &&) = default;
+  VideoPlayer &operator=(VideoPlayer &&) = default;
 
   void numberOfVideos();
   void showAllVideos();
-  void playVideo(const std::string& videoId);
+  void playVideo(const std::string &videoId);
   void stopVideo();
   void playRandomVideo();
   void pauseVideo();
   void continueVideo();
   void showPlaying();
-  void createPlaylist(const std::string& playlistName);
-  void addVideoToPlaylist(const std::string& playlistName, const std::string& videoId);
+  void createPlaylist(const std::string &playlistName);
+  void addVideoToPlaylist(const std::string &playlistName, const std::string &videoId);
   void showAllPlaylists();
-  void showPlaylist(const std::string& playlistName);
-  void removeFromPlaylist(const std::string& playlistName, const std::string& videoId);
-  void clearPlaylist(const std::string& playlistName);
-  void deletePlaylist(const std::string& playlistName);
-  void searchVideos(const std::string& searchTerm);
-  void searchVideosWithTag(const std::string& videoTag);
-  void flagVideo(const std::string& videoId);
-  void flagVideo(const std::string& videoId, const std::string& reason);
-  void allowVideo(const std::string& videoId);
+  void showPlaylist(const std::string &playlistName);
+  void removeFromPlaylist(const std::string &playlistName, const std::string &videoId);
+  void clearPlaylist(const std::string &playlistName);
+  void deletePlaylist(const std::string &playlistName);
+  void searchVideos(const std::string &searchTerm);
+  void searchVideosWithTag(const std::string &videoTag);
+  void flagVideo(const std::string &videoId);
+  void flagVideo(const std::string &videoId, const std::string &reason);
+  void allowVideo(const std::string &videoId);
 };
